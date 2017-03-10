eaa9c84a66dcff18629e56f54d2ab6df7e3a5bf7
Aaron Wilder <element1028@gmail.com>
2017-03-09 16:42:33 -0800

    all the stuff

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,37 @@ $(function() {
     url: 'https://www.codeschool.com/users/wildpow.json',
     dataType: 'jsonp',
     success: function(response) {
+      addCourses(response.courses.completed);
 
     }
 });
+
+function addCourses(courses) {
+
+  var $badges = $('#badges');
+
+  courses.forEach(function(course) {
+
+    var $course = $('<div />', {
+      'class': 'course'
+    }).appendTo($badges);
+
+    $('<h3 />', {
+      text: course.title
+    }).appendTo($course);
+
+    $('<img />', {
+      src: course.badge
+    }).appendTo($course);
+
+    $('<a />', {
+      'class': 'btn btn-primary',
+      target: '_blank',
+      href: course.url,
+      text: 'See Course'
+    }).appendTo($course);
+
+  })
+}
+
 });
